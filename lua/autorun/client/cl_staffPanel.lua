2f89e27da084f80ae62eac88e3ac4cc3efbc0168
NoIdeaIndustry <noideaindustry@gmail.com>
2022-08-30 18:47:46 +0100

    🏗️ One time display addon config + changed background color HUD

--- lua/autorun/client/cl_staffPanel.lua
+++ lua/autorun/client/cl_staffPanel.lua
@@ -6,7 +6,7 @@ function drawHUD()
     local screenWidth, screenHeight = ScrW(), ScrH()
     local hudWidth, hudHeight = screenWidth*.25, screenHeight*.25
 
-    surface.SetDrawColor(0,0,0,100)
+    surface.SetDrawColor(52,52,52,255)
     surface.DrawRect(screenWidth/2 - hudWidth/2, screenHeight/2 - hudHeight/2, hudWidth, hudHeight)
 
     surface.SetDrawColor(210,144,52,255)
--- lua/autorun/server/sv_staffPanel.lua
+++ lua/autorun/server/sv_staffPanel.lua
@@ -1,5 +1,14 @@
 include("autorun/sh_staffPanel.lua")
 
+print()print()
+print("------------------------------------")
+print("---------    " .. ADDON_CONFIG.name .. "    ---------")
+print("-------    " .. ADDON_CONFIG.author .. "    -------")
+print("------------------------------------")
+print()print()
+
+print("Successfully loaded " .. ADDON_CONFIG.name .. " ver: " .. ADDON_CONFIG.version)
+
 hook.Add("PlayerSay", "staffPanelPlayerSay", function(sender, text, teamChat)
     if text == "!admin" then 
         displayPrefix()
